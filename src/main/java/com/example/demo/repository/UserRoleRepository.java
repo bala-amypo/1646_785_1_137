da15cd0c4d4a37abaaec0c1c45b128903343c219
bala-amypo <bala@amypo.in>
2025-12-20 16:17:42 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/UserRoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.UserRole;
+
+
+
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+    
+}
